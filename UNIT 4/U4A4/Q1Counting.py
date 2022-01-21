44caf60f323c469a6291a3a48e85442b7ca267c6
AK-OCSB <94325120+AK-OCSB@users.noreply.github.com>
2022-01-21 08:37:44 -0500

    Update Q1Counting.py

--- UNIT 4/U4A4/Q1Counting.py
+++ UNIT 4/U4A4/Q1Counting.py
@@ -7,26 +7,26 @@
 # VARIABLE DEFINITION
 
 num = 0
-largenum = 0
-countnum = 0
+largeNum = 0
+countNum = 0
 
 
 # INPUT
 
 #Prompt user to input numbers.
-largenum = int(input("Please enter a large integer to count up to: "))
-countnum = int(input("Please enter a number to count by: "))
+largeNum = int(input("Please enter a large integer to count up to: "))
+countNum = int(input("Please enter a number to count by: "))
 
 
 # PROCESSING/OUTPUT
 
 
 #Counting up to the user number by 1.
-while num < largenum:
+while num < largeNum:
   print(num)
   num = num + 1
 
 #Counting down to zero from largenum by a count of the user choice.
-for i in range(num,0,-countnum):
+for i in range(num,0,-countNum):
   print(i)
 
