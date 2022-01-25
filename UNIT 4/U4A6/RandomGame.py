f3c675646e593da0ea92594ad20d77be728fb5e0
AK-OCSB <94325120+AK-OCSB@users.noreply.github.com>
2022-01-25 09:12:54 -0500

    U4A6 done.

--- UNIT 4/U4A6/RandomGame.py
+++ UNIT 4/U4A6/RandomGame.py
@@ -1,7 +1,7 @@
 # NAME OF AUTHOR:Ashley Koza  
 # NAME OF THE PROGRAM:Guessing Game
 # DATE OF CREATION:2022-01-24
-# PURPOSE OF PROGRAM:Practice what we have learned in each lesson. Practice random modules.
+# PURPOSE OF PROGRAM:Practice what we have learned in each lesson. Practice random and time modules. Pracice conditional loops.
 
 # VARIABLE DEFINITION
 
@@ -13,9 +13,15 @@ rand1 = 0
 userAnswer = 0
 counter = 0
 
+#PROCESSING
+
+
+
 #imports time and system specific parameters and functions
 import sys, time
  
+	
+	
 #define variable that will hold string
 def texttime(words):
 	
@@ -31,6 +37,8 @@ def texttime(words):
 		#pause of time between each character apearing
 		time.sleep(0.1)
  
+
+
  
 #display to user what this program is
 texttime("HI, welcome to the Random Number Game.\nThis game ask you to enter two numbers which will act as a range.\nThen you get to guess what number you think the computer choose.\n")
@@ -38,12 +46,18 @@ texttime("HI, welcome to the Random Number Game.\nThis game ask you to enter two
 #pause 2 seconds before showing the code below
 time.sleep(2)
 
+
+
 #prompt user if they would like to play or not
 playGame = input("\nWould you like to play.\nPlease enter 'y' for yes and 'n' for no:")
+
+
 
 #if no the game will end
 if playGame == 'n':
 	print("bye bye")
+	
+	
 	
 #if yes the game will run
 elif playGame == 'y':
@@ -52,7 +66,9 @@ elif playGame == 'y':
   number1 = int(input("Please enter an integer: "))
   number2 = int(input("Please enter another integer: "))
 
-#import random module  
+
+
+  #import random module  
   import random
   
   #define the max and min value as to not cause an error.
@@ -62,13 +78,18 @@ elif playGame == 'y':
   #Sets a range, then picks a random number between said range.
   rand1 = random.randint(minNumber, maxNumber)
 
+	
+	
   #repeats the process only while the user's answer is not equal to the random number
   while userAnswer != rand1:
     userAnswer = int(input("Please guess a number you think the computer choose: "))
 
-#add 1 to counter
+    #add 1 to counter
     counter += 1
-#if user answer equal to random number, inform user and display how many tries it took them
+
+
+
+    #if user answer equal to random number, inform user and display how many tries it took them
     if userAnswer ==rand1:
     	print("Wow, you guessed the number, it took you" , counter , "try(s) to get it right.")
 
