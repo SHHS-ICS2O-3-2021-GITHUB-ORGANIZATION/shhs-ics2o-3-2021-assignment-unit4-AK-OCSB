9dc95e4f87a256c4dec7522e6efdaed1db04e8a8
Andrew <Andrew@10.0.0.32>
2022-01-26 15:06:28 -0500

    U4A6 done.

--- UNIT 4/U4A6/RandomGame.py
+++ UNIT 4/U4A6/RandomGame.py
@@ -19,7 +19,6 @@ counter = 1
 
 #imports time and system specific parameters and functions
 import sys, time
- 
 	
 	
 #define variable that will hold string
@@ -35,11 +34,11 @@ def texttime(words):
 		sys.stdout.flush()
 		
 		#pause of time between each character apearing
-		time.sleep(0.1)
- 
+		time.sleep(0.08)
 
 
- 
+
+
 #display to user what this program is
 texttime("HI, welcome to the Random Number Game.\nThis game ask you to enter two numbers which will act as a range.\nThen you get to guess what number you think the computer choose.\n")
 
@@ -62,48 +61,49 @@ if playGame == 'n':
 #if yes the game will run
 elif playGame == 'y':
 	
-  #promt user for 2 intergers
-  number1 = int(input("Please enter an integer: "))
-  number2 = int(input("Please enter another integer: "))
-
-
-
-  #import random module  
-  import random
-  
-  #define the max and min value as to not cause an error.
-  maxNumber = max(number1 , number2)
-  minNumber = min(number1 , number2)
+	#promt user for 2 intergers
+	number1 = int(input("Please enter an integer: "))
+	number2 = int(input("Please enter another integer: "))
 	
-  #Sets a range, then picks a random number between said range.
-  rand1 = random.randint(minNumber, maxNumber)
-  
-  
-  #prompt user to guess a number
-  userAnswer = int(input("Please guess a number you think the computer choose: "))
-  
-  
-  
-  #if user's answer is equal to random number display they are correct
-  if userAnswer ==rand1:
-  	print("Wow, you guessed the number, it took you 1 try to get it right.")
-  
-  
-  
-  
-  #repeats the process only while the user's answer is not equal to the random number
-  while userAnswer != rand1:
-  
-  	
-  	#prompt user to try again
-  	userAnswer = int(input("Please try again:"))
-  	
-  	#adds 1 to counter
-  	counter += 1
-  	
-  	#if user's answer is equal to random number display they are correct
-  	if userAnswer ==rand1:
-  		print("Wow, you guessed the number, it took you" , counter ,  "tries to get it right.")
-  
+	
+	#import random module  
+	import random
+	
+	
+	#define the max and min value as to not cause an error.
+	maxNumber = max(number1 , number2)
+	minNumber = min(number1 , number2)
+	
+	
+	#Sets a range, then picks a random number between said range.
+	rand1 = random.randint(minNumber, maxNumber)
+	
+	#prompt user to guess a number
+	userAnswer = int(input("Please guess a number you think the computer choose: "))
+	
+	
+	
+	#if user's answer is equal to random number display they are correct
+	if userAnswer ==rand1:
+		print("Wow, you guessed the number, it took you 1 try to get it right.")
+	
+	
+	
+	#repeats the process only while the user's answer is not equal to the random number
+	while userAnswer != rand1:
+		
+		
+		
+		#prompt user to try again
+		userAnswer = int(input("Please try again:"))
+		
+		
+		#adds 1 to counter
+		counter += 1
+		
+		
+		#if user's answer is equal to random number display they are correct
+	if userAnswer ==rand1:
+		print("Wow, you guessed the number, it took you" , counter ,  "tries to get it right.")
 
 
